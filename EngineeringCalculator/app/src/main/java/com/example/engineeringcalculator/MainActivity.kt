6b0dd2127c42a16fed12e593b698e6659fc09e98
Elizabeth <bellator.379@gmail.com>
2024-04-25 05:52:03 +0300

    Theme choose activity. Theme changing function

--- EngineeringCalculator/app/src/main/java/com/example/engineeringcalculator/MainActivity.kt
+++ EngineeringCalculator/app/src/main/java/com/example/engineeringcalculator/MainActivity.kt
@@ -1,30 +1,36 @@
 package com.example.engineeringcalculator
 
 import android.annotation.SuppressLint
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
 import android.content.Context
 import android.content.Intent
-import android.content.res.Configuration
 import android.content.pm.ActivityInfo
-import android.text.method.ScrollingMovementMethod
-import android.widget.Button
-import android.widget.Toast
-import android.os.Vibrator
-import com.example.engineeringcalculator.calculations.Calculator
-import android.view.GestureDetector
-import android.view.MotionEvent
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.GestureDetectorCompat
-import com.example.engineeringcalculator.calculations.Operations
+import android.content.res.Configuration
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Bundle
+import android.os.Vibrator
+import android.preference.PreferenceManager
+import android.text.method.ScrollingMovementMethod
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.GestureDetectorCompat
+import com.example.engineeringcalculator.calculations.Calculator
+import com.example.engineeringcalculator.calculations.Operations
 import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity(), Connector, SensorEventListener {
+    val db = Firebase.firestore
+
     private var sensorManager: SensorManager? = null
     private var accelerometer: Sensor? = null
     private var lastTime: Long = 0
@@ -48,6 +54,43 @@ class MainActivity : AppCompatActivity(), Connector, SensorEventListener {
     private lateinit var vibrator : Vibrator
     private lateinit var lSwipeDetector: GestureDetectorCompat
     private lateinit var mainLayout: ConstraintLayout
+
+    private lateinit var btn_0: Button
+    private lateinit var btn_1: Button
+    private lateinit var btn_2: Button
+    private lateinit var btn_3: Button
+    private lateinit var btn_4: Button
+    private lateinit var btn_5: Button
+    private lateinit var btn_6: Button
+    private lateinit var btn_7: Button
+    private lateinit var btn_8: Button
+    private lateinit var btn_9: Button
+    private lateinit var btn_dot: Button
+    private lateinit var btn_back: Button
+    private lateinit var btn_ac: Button
+    private lateinit var btn_plus: Button
+    private lateinit var btn_minus: Button
+    private lateinit var btn_multiply: Button
+    private lateinit var btn_division: Button
+    private lateinit var btn_equal: Button
+    private lateinit var btn_parenthesis_l: Button
+    private lateinit var btn_parenthesis_r: Button
+
+    private lateinit var btn_sin: Button
+    private lateinit var btn_cos: Button
+    private lateinit var btn_tan: Button
+    private lateinit var btn_rad: Button
+    private lateinit var btn_sqrt: Button
+    private lateinit var btn_ln: Button
+    private lateinit var btn_lg: Button
+    private lateinit var btn_log: Button
+    private lateinit var btn_powerMinOne: Button
+    private lateinit var btn_expPowX: Button
+    private lateinit var btn_square: Button
+    private lateinit var btn_power: Button
+    private lateinit var btn_module: Button
+    private lateinit var btn_pi: Button
+    private lateinit var btn_exp: Button
 
 
     private fun changeMode() {
@@ -197,6 +240,72 @@ class MainActivity : AppCompatActivity(), Connector, SensorEventListener {
         }
     }
 
+    private fun applyTheme(themeId: Int) {
+        val orientation = resources.configuration.orientation
+        when (themeId) {
+            1 -> {
+                window.statusBarColor = ContextCompat.getColor(this, R.color.base)
+
+                setButtonOneColor(R.color.orange)
+                setButtonColor(R.color.tomato)
+                setNumberColor(R.color.white)
+                setOperationColor(R.color.light_orange)
+
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    setFuncColor(R.color.orange)
+                }
+            }
+            2 -> {
+                window.statusBarColor = ContextCompat.getColor(this, R.color.green)
+
+                setButtonOneColor(R.color.gr_butt)
+                setButtonColor(R.color.breeze)
+                setNumberColor(R.color.orange)
+                setOperationColor(R.color.darkorange)
+
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    setFuncColor(R.color.darkbreeze)
+                }
+            }
+            3 -> {
+                window.statusBarColor = ContextCompat.getColor(this, R.color.breeze)
+
+                setButtonOneColor(R.color.br_butt)
+                setButtonColor(R.color.green)
+                setNumberColor(R.color.yellow)
+                setOperationColor(R.color.darkyellow)
+
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    setFuncColor(R.color.darkgreen)
+                }
+            }
+            4 -> {
+                window.statusBarColor = ContextCompat.getColor(this, R.color.moon)
+
+                setButtonOneColor(R.color.mo_butt)
+                setButtonColor(R.color.purple)
+                setNumberColor(R.color.sky)
+                setOperationColor(R.color.darksky)
+
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    setFuncColor(R.color.darkpurple)
+                }
+            }
+            else -> {
+                window.statusBarColor = ContextCompat.getColor(this, R.color.base)
+
+                setButtonColor(R.color.blue)
+                setNumberColor(R.color.pink)
+                setOperationColor(R.color.darkpink)
+
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    setFuncColor(R.color.violet)
+                }
+            }
+        }
+    }
+
+
     fun getinputText(): TextView {
         return inputText
     }
@@ -214,4 +323,151 @@ class MainActivity : AppCompatActivity(), Connector, SensorEventListener {
         val version = pInfo.versionName
         return "demo" in version
     }
+
+    private fun saveThemeToSharedPreferences(themeId: Int) {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = preferences.edit()
+        editor.putInt("theme_id", themeId)
+        editor.apply()
+    }
+
+    private fun applySavedTheme() {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        val savedThemeId = preferences.getInt("theme_id", 1)
+
+        setAppTheme(savedThemeId)
+        applyTheme(savedThemeId)
+
+        db.collection("theme").document("current")
+            .get()
+            .addOnSuccessListener { documentSnapshot ->
+                val dbThemeId = documentSnapshot.getLong("theme_id")?.toInt() ?: return@addOnSuccessListener
+
+                if (dbThemeId != savedThemeId) {
+                    saveThemeToSharedPreferences(dbThemeId)
+                    setAppTheme(dbThemeId)
+                    applyTheme(dbThemeId)
+                }
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this@MainActivity, "Failed to read theme ID from Firestore!", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun setAppTheme(themeId: Int) {
+        when (themeId) {
+            1 -> window.statusBarColor = ContextCompat.getColor(this, R.color.base)
+            2 -> window.statusBarColor = ContextCompat.getColor(this, R.color.green)
+            3 -> window.statusBarColor = ContextCompat.getColor(this, R.color.breeze)
+            4 -> window.statusBarColor = ContextCompat.getColor(this, R.color.moon)
+
+        }
+    }
+
+    fun setButtonOneColor(colorResId: Int) {
+        btn_theme = findViewById(R.id.btn_theme)
+        btn_theme.setBackgroundColor(getResources().getColor(colorResId))
+        btn_2nd = findViewById(R.id.btn_2nd)
+        btn_2nd.setBackgroundColor(getResources().getColor(colorResId))
+        btn_equal = findViewById(R.id.btn_equal)
+        btn_equal.setBackgroundColor(getResources().getColor(colorResId))
+    }
+
+    fun setButtonColor(colorResId: Int) {
+        btn_ac = findViewById(R.id.btn_ac)
+        btn_ac.setBackgroundColor(getResources().getColor(colorResId))
+        btn_back = findViewById(R.id.btn_back)
+        btn_back.setBackgroundColor(getResources().getColor(colorResId))
+    }
+
+    fun setNumberColor(colorResId: Int) {
+        btn_0 = findViewById(R.id.btn_0)
+        btn_0.setBackgroundColor(getResources().getColor(colorResId))
+        btn_1 = findViewById(R.id.btn_1)
+        btn_1.setBackgroundColor(getResources().getColor(colorResId))
+        btn_2 = findViewById(R.id.btn_2)
+        btn_2.setBackgroundColor(getResources().getColor(colorResId))
+        btn_3 = findViewById(R.id.btn_3)
+        btn_3.setBackgroundColor(getResources().getColor(colorResId))
+        btn_4 = findViewById(R.id.btn_4)
+        btn_4.setBackgroundColor(getResources().getColor(colorResId))
+        btn_5 = findViewById(R.id.btn_5)
+        btn_5.setBackgroundColor(getResources().getColor(colorResId))
+        btn_6 = findViewById(R.id.btn_6)
+        btn_6.setBackgroundColor(getResources().getColor(colorResId))
+        btn_7 = findViewById(R.id.btn_7)
+        btn_7.setBackgroundColor(getResources().getColor(colorResId))
+        btn_8 = findViewById(R.id.btn_8)
+        btn_8.setBackgroundColor(getResources().getColor(colorResId))
+        btn_9 = findViewById(R.id.btn_9)
+        btn_9.setBackgroundColor(getResources().getColor(colorResId))
+        btn_dot = findViewById(R.id.btn_dot)
+        btn_dot.setBackgroundColor(getResources().getColor(colorResId))
+    }
+
+    fun setOperationColor(colorResId: Int) {
+        btn_multiply = findViewById(R.id.btn_multiply)
+        btn_multiply.setBackgroundColor(getResources().getColor(colorResId))
+        btn_minus = findViewById(R.id.btn_minus)
+        btn_minus.setBackgroundColor(getResources().getColor(colorResId))
+        btn_plus = findViewById(R.id.btn_plus)
+        btn_plus.setBackgroundColor(getResources().getColor(colorResId))
+        btn_division = findViewById(R.id.btn_division)
+        btn_division.setBackgroundColor(getResources().getColor(colorResId))
+        btn_parenthesis_l = findViewById(R.id.btn_parenthesis_l)
+        btn_parenthesis_l.setBackgroundColor(getResources().getColor(colorResId))
+        btn_parenthesis_r = findViewById(R.id.btn_parenthesis_r)
+        btn_parenthesis_r.setBackgroundColor(getResources().getColor(colorResId))
+    }
+
+    fun setFuncColor(colorResId: Int) {
+        btn_sin = findViewById(R.id.btn_sin)
+        btn_sin.setBackgroundColor(getResources().getColor(colorResId))
+
+        btn_cos = findViewById(R.id.btn_cos)
+        btn_cos.setBackgroundColor(getResources().getColor(colorResId))
+
+        btn_tan = findViewById(R.id.btn_tan)
+        btn_tan.setBackgroundColor(getResources().getColor(colorResId))
+
+        btn_rad = findViewById(R.id.btn_rad)
+        btn_rad.setBackgroundColor(getResources().getColor(colorResId))
+
+        btn_sqrt = findViewById(R.id.btn_sqrt)
+        btn_sqrt.setBackgroundColor(getResources().getColor(colorResId))
+
+        btn_ln = findViewById(R.id.btn_ln)
+        btn_ln.setBackgroundColor(getResources().getColor(colorResId))
+
+        btn_lg = findViewById(R.id.btn_lg)
+        btn_lg.setBackgroundColor(getResources().getColor(colorResId))
+
+
+        btn_log = findViewById(R.id.btn_log)
+        btn_log.setBackgroundColor(getResources().getColor(colorResId))
+
+        btn_powerMinOne = findViewById(R.id.btn_powerMinOne)
+        btn_powerMinOne.setBackgroundColor(getResources().getColor(colorResId))
+
+        btn_expPowX = findViewById(R.id.btn_expPowX)
+        btn_expPowX.setBackgroundColor(getResources().getColor(colorResId))
+
+        btn_square = findViewById(R.id.btn_square)
+        btn_square.setBackgroundColor(getResources().getColor(colorResId))
+
+        btn_power = findViewById(R.id.btn_power)
+        btn_power.setBackgroundColor(getResources().getColor(colorResId))
+
+        btn_module = findViewById(R.id.btn_module)
+        btn_module.setBackgroundColor(getResources().getColor(colorResId))
+
+        btn_pi = findViewById(R.id.btn_pi)
+        btn_pi.setBackgroundColor(getResources().getColor(colorResId))
+
+        btn_exp = findViewById(R.id.btn_exp)
+        btn_exp.setBackgroundColor(getResources().getColor(colorResId))
+    }
+
+
 }
--- EngineeringCalculator/app/src/main/java/com/example/engineeringcalculator/ThemeActivity.kt
+++ EngineeringCalculator/app/src/main/java/com/example/engineeringcalculator/ThemeActivity.kt
@@ -2,12 +2,19 @@ package com.example.engineeringcalculator
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Handler
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.engineeringcalculator.R
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class ThemeActivity : AppCompatActivity() {
+
+    val db = Firebase.firestore
 
     private lateinit var btn_theme1: Button
     private lateinit var btn_theme2: Button
@@ -21,19 +28,20 @@ class ThemeActivity : AppCompatActivity() {
 
         btn_theme1 = findViewById(R.id.btn_theme1)
         btn_theme1.setOnClickListener{
-
+            changeTheme(1)
         }
         btn_theme2 = findViewById(R.id.btn_theme2)
         btn_theme2.setOnClickListener{
+            changeTheme(2)
 
         }
         btn_theme3 = findViewById(R.id.btn_theme3)
         btn_theme3.setOnClickListener{
-
+            changeTheme(3)
         }
         btn_theme4 = findViewById(R.id.btn_theme4)
         btn_theme4.setOnClickListener{
-
+            changeTheme(4)
         }
 
     }
@@ -43,5 +51,76 @@ class ThemeActivity : AppCompatActivity() {
         startActivity(i)
         finish()
         super.onBackPressed()
+    }
+
+    private fun changeTheme(themeId: Int) {
+        when (themeId) {
+            1 -> {
+                window.statusBarColor = ContextCompat.getColor(this, R.color.base)
+                val constraintLayout: ConstraintLayout = findViewById(R.id.laymain)
+                val gradientDrawable = ContextCompat.getDrawable(this, R.drawable.cool_gradient)
+                constraintLayout.background = gradientDrawable
+
+                btn_theme1 = findViewById(R.id.btn_theme1)
+                btn_theme1.setBackgroundColor(ContextCompat.getColor(this, R.color.orange))
+                btn_theme2 = findViewById(R.id.btn_theme2)
+                btn_theme2.setBackgroundColor(ContextCompat.getColor(this, R.color.orange))
+                btn_theme3 = findViewById(R.id.btn_theme3)
+                btn_theme3.setBackgroundColor(ContextCompat.getColor(this, R.color.orange))
+                btn_theme4 = findViewById(R.id.btn_theme4)
+                btn_theme4.setBackgroundColor(ContextCompat.getColor(this, R.color.orange))
+
+            }
+
+            2 -> {
+                window.statusBarColor = ContextCompat.getColor(this, R.color.green)
+                val constraintLayout: ConstraintLayout = findViewById(R.id.laymain)
+                val gradientDrawable = ContextCompat.getDrawable(this, R.drawable.green_gradient)
+                constraintLayout.background = gradientDrawable
+
+                btn_theme1 = findViewById(R.id.btn_theme1)
+                btn_theme1.setBackgroundColor(ContextCompat.getColor(this, R.color.gr_butt))
+                btn_theme2 = findViewById(R.id.btn_theme2)
+                btn_theme2.setBackgroundColor(ContextCompat.getColor(this, R.color.gr_butt))
+                btn_theme3 = findViewById(R.id.btn_theme3)
+                btn_theme3.setBackgroundColor(ContextCompat.getColor(this, R.color.gr_butt))
+                btn_theme4 = findViewById(R.id.btn_theme4)
+                btn_theme4.setBackgroundColor(ContextCompat.getColor(this, R.color.gr_butt))
+
+            }
+
+            3 -> {
+                window.statusBarColor = ContextCompat.getColor(this, R.color.breeze)
+                val constraintLayout: ConstraintLayout = findViewById(R.id.laymain)
+                val gradientDrawable = ContextCompat.getDrawable(this, R.drawable.blue_gradient)
+                constraintLayout.background = gradientDrawable
+
+                btn_theme1 = findViewById(R.id.btn_theme1)
+                btn_theme1.setBackgroundColor(ContextCompat.getColor(this, R.color.br_butt))
+                btn_theme2 = findViewById(R.id.btn_theme2)
+                btn_theme2.setBackgroundColor(ContextCompat.getColor(this, R.color.br_butt))
+                btn_theme3 = findViewById(R.id.btn_theme3)
+                btn_theme3.setBackgroundColor(ContextCompat.getColor(this, R.color.br_butt))
+                btn_theme4 = findViewById(R.id.btn_theme4)
+                btn_theme4.setBackgroundColor(ContextCompat.getColor(this, R.color.br_butt))
+
+            }
+
+            4 -> {
+                window.statusBarColor = ContextCompat.getColor(this, R.color.moon)
+                val constraintLayout: ConstraintLayout = findViewById(R.id.laymain)
+                val gradientDrawable = ContextCompat.getDrawable(this, R.drawable.yellow_gradient)
+                constraintLayout.background = gradientDrawable
+
+                btn_theme1 = findViewById(R.id.btn_theme1)
+                btn_theme1.setBackgroundColor(ContextCompat.getColor(this, R.color.mo_butt))
+                btn_theme2 = findViewById(R.id.btn_theme2)
+                btn_theme2.setBackgroundColor(ContextCompat.getColor(this, R.color.mo_butt))
+                btn_theme3 = findViewById(R.id.btn_theme3)
+                btn_theme3.setBackgroundColor(ContextCompat.getColor(this, R.color.mo_butt))
+                btn_theme4 = findViewById(R.id.btn_theme4)
+                btn_theme4.setBackgroundColor(ContextCompat.getColor(this, R.color.mo_butt))
+            }
+        }
     }
 }
