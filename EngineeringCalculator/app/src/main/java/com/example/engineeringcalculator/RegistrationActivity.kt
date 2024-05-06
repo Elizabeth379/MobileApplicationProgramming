17dfb92d34cc2ec68399bb8c8f4301f9b73489da
Elizabeth <bellator.379@gmail.com>
2024-05-07 01:04:32 +0300

    Authetication and registration base created

--- EngineeringCalculator/app/src/main/java/com/example/engineeringcalculator/RegistrationActivity.kt
+++ EngineeringCalculator/app/src/main/java/com/example/engineeringcalculator/RegistrationActivity.kt
@@ -1,29 +1,25 @@
 package com.example.engineeringcalculator
-
 import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Handler
-import android.view.View
+import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RelativeLayout
 import android.widget.Toast
-import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
 
+class RegistrationActivity : AppCompatActivity() {
 
-class AuthenticationActivity : AppCompatActivity() {
     private val db = Firebase.firestore
     private var passCode: String = ""
 
     private var isHaveBiometric: Boolean = true
     private lateinit var biometricPrompt: BiometricPrompt
     private lateinit var activityM: Intent
-    private lateinit var activityR: Intent
+    private lateinit var activityA: Intent
 
     private lateinit var passwordET: EditText
     private lateinit var bOne: Button
@@ -37,16 +33,15 @@ class AuthenticationActivity : AppCompatActivity() {
     private lateinit var bNine: Button
     private lateinit var bZero: Button
     private lateinit var bClear: Button
-
-
+    private lateinit var bOK: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.authentication)
+        setContentView(R.layout.registration)
+
 
         activityM = Intent(this, MainActivity::class.java)
-        activityR = Intent(this, RegistrationActivity::class.java)
+        activityA = Intent(this, AuthenticationActivity::class.java)
 
-        initializeButtons()
         passwordET = findViewById(R.id.password_ed)
         bOne = findViewById(R.id.b_one)
         bTwo = findViewById(R.id.b_two)
@@ -59,12 +54,8 @@ class AuthenticationActivity : AppCompatActivity() {
         bNine = findViewById(R.id.b_nine)
         bZero = findViewById(R.id.b_zero)
         bClear = findViewById(R.id.b_clear)
+        bOK = findViewById(R.id.regButton)
 
-        checkBiometricInDevice() // Тут проверяем есть ли вход по биометрии у пользователя
-
-        if (isHaveBiometric) { // Тут проверка
-            initializeButtonForBiometric() // тут инициализируем кнопку входа по биометрии
-        }
 
         bOne.setOnClickListener {
             val value: String = passwordET.text.toString()
@@ -125,73 +116,13 @@ class AuthenticationActivity : AppCompatActivity() {
             }
         }
 
+        bOK.setOnClickListener {
+
+        }
+
         applySavedTheme()
-
     }
 
-
-    private fun initializeButtons() {
-        val logInButton = findViewById<Button>(R.id.LogIn)
-        val signInButton = findViewById<Button>(R.id.SignIn)
-
-        signInButton.setOnClickListener {
-            startActivity(activityR)
-        }
-
-        logInButton.setOnClickListener {
-            //checkPassword()
-        }
-    }
-
-
-    private fun checkBiometricInDevice() {
-        val biometricManager = BiometricManager.from(this)
-        val buttonOpenBiometric = findViewById<Button>(R.id.biometrics)
-
-        when (biometricManager.canAuthenticate()) {
-            BiometricManager.BIOMETRIC_SUCCESS -> {
-                buttonOpenBiometric.visibility = View.VISIBLE
-                isHaveBiometric = true
-            }
-
-            else -> {
-                buttonOpenBiometric.visibility = View.GONE
-                isHaveBiometric = false
-            }
-        }
-    }
-
-    private fun initializeButtonForBiometric() {
-        val buttonBiometric = findViewById<Button>(R.id.biometrics)
-
-        biometricPrompt = BiometricPrompt(this@AuthenticationActivity, ContextCompat.getMainExecutor(this), object:androidx.biometric.BiometricPrompt.AuthenticationCallback() {
-            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-                val cryptoObject = result.cryptoObject
-                startActivity(activityM)
-            }
-
-            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-                Toast.makeText(this@AuthenticationActivity, "Authentication error", Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onAuthenticationFailed() {
-                Toast.makeText(this@AuthenticationActivity, "Error", Toast.LENGTH_SHORT).show()
-            }
-        }
-        )
-
-        buttonBiometric.setOnClickListener {
-            biometricPrompt.authenticate(createBiometricPromptInfo())
-        }
-
-    }
-
-    private fun createBiometricPromptInfo(): BiometricPrompt.PromptInfo {
-        return BiometricPrompt.PromptInfo.Builder()
-            .setTitle("Authorization")
-            .setNegativeButtonText("Cancel")
-            .build()
-    }
 
     private fun applySavedTheme() {
 
@@ -202,23 +133,22 @@ class AuthenticationActivity : AppCompatActivity() {
                 changeTheme(dbThemeId)
             }
             .addOnFailureListener { e ->
-                Toast.makeText(this@AuthenticationActivity, "Failed to read theme ID from Firestore!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@RegistrationActivity, "Failed to read theme ID from Firestore!", Toast.LENGTH_SHORT).show()
             }
     }
-
 
     private fun changeTheme(themeId: Int) {
         when (themeId) {
             1 -> {
                 window.statusBarColor = ContextCompat.getColor(this, R.color.base)
-                val relativeLayout: RelativeLayout = findViewById(R.id.auth)
+                val relativeLayout: RelativeLayout = findViewById(R.id.reg)
                 val gradientDrawable = ContextCompat.getDrawable(this, R.drawable.cool_gradient)
                 relativeLayout.background = gradientDrawable
             }
 
             2 -> {
                 window.statusBarColor = ContextCompat.getColor(this, R.color.green)
-                val relativeLayout: RelativeLayout = findViewById(R.id.auth)
+                val relativeLayout: RelativeLayout = findViewById(R.id.reg)
                 val gradientDrawable = ContextCompat.getDrawable(this, R.drawable.green_gradient)
                 relativeLayout.background = gradientDrawable
 
@@ -226,7 +156,7 @@ class AuthenticationActivity : AppCompatActivity() {
 
             3 -> {
                 window.statusBarColor = ContextCompat.getColor(this, R.color.breeze)
-                val relativeLayout: RelativeLayout = findViewById(R.id.auth)
+                val relativeLayout: RelativeLayout = findViewById(R.id.reg)
                 val gradientDrawable = ContextCompat.getDrawable(this, R.drawable.blue_gradient)
                 relativeLayout.background = gradientDrawable
 
@@ -234,7 +164,7 @@ class AuthenticationActivity : AppCompatActivity() {
 
             4 -> {
                 window.statusBarColor = ContextCompat.getColor(this, R.color.moon)
-                val relativeLayout: RelativeLayout = findViewById(R.id.auth)
+                val relativeLayout: RelativeLayout = findViewById(R.id.reg)
                 val gradientDrawable = ContextCompat.getDrawable(this, R.drawable.yellow_gradient)
                 relativeLayout.background = gradientDrawable
 
@@ -242,4 +172,6 @@ class AuthenticationActivity : AppCompatActivity() {
             }
         }
     }
+
+
 }
